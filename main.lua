a2df1d002f156295602fc1fc3ec616e7bec9d784
Samuelmooregaming-games <samweidmanqwe@gmail.com>
2025-04-14 21:25:17 -0400

    initial setuo is complete test ready

---
+++ main.lua
@@ -0,0 +1,19 @@
+if arg[2] == "debug" then
+    require("lldebugger").start()
+end
+
+
+    function love.draw()
+    love.graphics.print("hello world!", 400, 300)
+    end
+print("hello world")
+
+    local love_errorhandler = love.errorhandler
+
+function love.errorhandler(msg)
+    if lldebugger then
+        error(msg, 2)
+    else
+        return love_errorhandler(msg)
+    end
+end
